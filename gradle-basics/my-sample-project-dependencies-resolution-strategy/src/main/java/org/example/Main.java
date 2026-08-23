3c692cb835f2fcfce003338f94f3195b52764b9f
jws.anil@gmail.com <anil.jws@outlook.com>
2026-08-24 00:24:35 +0530

    initial commit

---
+++ gradle-basics/my-sample-project-dependencies-resolution-strategy/src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import org.apache.commons.lang3.StringUtils;
+
+
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+
+            System.out.println("i = " + i);
+
+        }
+
+
+        String name = "Gradle";
+        System.out.println(
+                "output:" +
+                        StringUtils.isNotBlank(name));
+
+        }
+
+        // output: true
+    }
